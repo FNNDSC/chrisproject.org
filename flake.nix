2444d832bc0a3e7c64a17c9d5c9bc3496487d9f4
Jennings Zhang <dev@sl.jennin.xyz>
2026-04-24 00:56:28 -0400

    Remove corepack, install pnpm directly in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,8 @@
   description = "pnpm development environment for chrisproject.org website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -15,12 +12,19 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        fhs = pkgs.buildFHSEnv {
-          name = "corepack";
+        lib = pkgs.lib;
+        fhs = pkgs.buildFHSEnv ({
+          name = "pnpm";
           targetPkgs = pkgs: with pkgs; [
-            corepack
+            pnpm
+            nodejs-slim_24
           ];
-        };
+        } // (
+        let
+          runScript = "${builtins.getEnv "SHELL"}";
+        in lib.optionalAttrs (runScript != "") {
+          inherit runScript;
+        }));
       in {
         devShells.default = fhs.env;
       });
